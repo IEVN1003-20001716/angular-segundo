3380f2760f1b303c5dcb577c7c685346549a411e
IEVN1003-20001716 <rlystudios21@gmail.com>
2024-10-09 21:42:29 -0600

    angular-Segundo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Ejemplo1Component } from "./formulario/ejemplo1/ejemplo1.component";
+import { ZodiacoComponent } from "./formulario/zodiaco/zodiaco.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Ejemplo1Component, ZodiacoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/formulario/zodiaco/zodiaco.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-zodiaco',
+  standalone: true,
+  imports: [],
+  templateUrl: './zodiaco.component.html',
+  styleUrl: './zodiaco.component.css'
+})
+export class ZodiacoComponent {
+  
+}
